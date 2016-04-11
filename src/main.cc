025f77664ed03133e09f9b637e937c992d92262d
Nicolas Chariglione <nico.chari@gmail.com>
2016-04-12 01:40:45 +0200

    [PARSE] Add getters for parser values

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "option-parser.hh"
 
-int main(int argc, char **argv)
-{
-  std::shared_ptr<OptionParser> parser =
-    std::make_shared<OptionParser>(OptionParser(argc, argv));
-  parser->parse_options();
+  int main(int argc, char **argv)
+  {
+    beetrail::OptionParser parser(argc, argv);
+    parser.parse_options();
 
-  return 0;
-}
+    return 0;
+  }
--- src/option-parser.hh
+++ src/option-parser.hh
@@ -2,25 +2,37 @@
 
 #include <boost/program_options.hpp>
 
-namespace bpo = boost::program_options;
-
-enum error_code
+namespace beetrail
 {
-  good = 0,
-  error = 1
-};
+  namespace bpo = boost::program_options;
+
+  enum error_code
+  {
+    good = 0,
+    error = 1
+  };
 
 
-class OptionParser
-{
-  public:
-    /* Fields */
-    bpo::options_description desc; /* Contains options description */
-    bpo::variables_map vm; /* Map containing arguments values after parsing */
-    int argc; /* Number of arguments */
-    char **argv; /* Arguments */
+  class OptionParser
+  {
+    public:
+      /* Fields */
+      bpo::options_description desc; /* Contains options description */
+      bpo::variables_map vm; /* Map containing arguments values after parsing */
+      int argc; /* Number of arguments */
+      char **argv; /* Arguments */
 
-    OptionParser(int p_argc, char **p_argv); /* Constructor */
-    void action_help(int error_code); /* Displays help and exits */
-    int parse_options(); /* Parse options and put results in the class fields */
-};
+      OptionParser(int p_argc, char **p_argv); /* Constructor */
+      int parse_options(); /* Parse options and put results in the class fields */
+      void action_help(int error_code); /* Displays help and exits */
+
+      /* Parser values getters */
+      int help_called();
+      int print_called();
+      int time_called();
+      std::string topology();
+      std::string video_input();
+      std::string object_input();
+      int frames_per_second();
+  };
+}
