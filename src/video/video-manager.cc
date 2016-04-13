0ba959031fd7f2623d6c6de69e92426c67c28c55
Charles Yaiche <yaiche_c@epita.fr>
2016-04-13 17:48:02 +0200

    [VIDEO] Indent video-manager.cc

--- src/video/video-manager.cc
+++ src/video/video-manager.cc
@@ -22,51 +22,52 @@ namespace beetrail
 
 
   std::string
-  VideoManager::name_window_get()
-  {
-    return name_window_;
-  }
+    VideoManager::name_window_get()
+    {
+      return name_window_;
+    }
 
   bool
-  VideoManager::check_file()
-  {
-    return capture_.isOpened();
-  }
+    VideoManager::check_file()
+    {
+      return capture_.isOpened();
+    }
 
 
   Mat
-  VideoManager::frame_get()
-  {
-    Mat frame;
-    capture_ >> frame;
-    return frame;
-  }
+    VideoManager::frame_get()
+    {
+      Mat frame;
+      capture_ >> frame;
+      return frame;
+    }
 
   VideoCapture
-  VideoManager::capture_get()
-  {
-    return capture_;
-  }
+    VideoManager::capture_get()
+    {
+      return capture_;
+    }
 
   void
-  VideoManager::draw_square(int size, int x, int y, Mat frame)
-  {
-    for (int i = x; i < x + size; i ++)
-      for (int j = y; j < y + size; j ++)
-      {
-	auto s = frame.size();
-	std::cout << "i : " << i << ", j :" << j << "\n";
-	std::cout << "x : " << x << ", y :" << y << "\n";
-	std::cout << "size : " << size << "\n";
-	if (i < s.height && j < s.width)
-	{
-	  Vec3b color = frame.at<Vec3b>(Point(i, j));
-	  color.val[0] = 0;
-	  color.val[1] = 255;
-	  color.val[2] = 0;
-	  frame.at<Vec3b>(Point(i, j)) = color;
+    VideoManager::draw_square(int size, int x, int y, Mat frame)
+    {
+      for (int i = x; i < x + size; i ++)
+        for (int j = y; j < y + size; j ++)
+        {
+          auto s = frame.size();
 
-	}
-      }
-  }
+          std::cout << "i : " << i << ", j :" << j << "\n";
+          std::cout << "x : " << x << ", y :" << y << "\n";
+          std::cout << "size : " << size << "\n";
+          if (i < s.height && j < s.width)
+          {
+            Vec3b color = frame.at<Vec3b>(Point(i, j));
+            color.val[0] = 0;
+            color.val[1] = 255;
+            color.val[2] = 0;
+            frame.at<Vec3b>(Point(i, j)) = color;
+
+          }
+        }
+    }
 }
