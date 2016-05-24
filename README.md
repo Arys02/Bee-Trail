d7163c9f8b98f8bdfebd294edc47dc9602dd3337
Nicolas Chariglione <nico.chari@gmail.com>
2016-05-24 23:09:16 +0200

    [FACTO] Start refactoring code. Move parsing to main. Pso init has to bedone now

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Bee-Trail
-Bee Trail is a software simulating particls swarm and use it to tracks objects in video.
-The use of particls swarm algorithm in artificial intelligence is inspired by
-insects like bee or ants, indeed when many basic intelligence act together, it
-is possible to see more advenced behavior.
+
+Bee-Trail is a software simulating a swarm of particules (PSO) and using it to
+track objects on video.
+
+Call the binary with -h to get some help.
--- src/main.cc
+++ src/main.cc
@@ -11,3 +11,76 @@ int main(int argc, char **argv)
   beetrail::OptionManager manager(parser);
   return manager.action();
 }
+
+
+
+
+//private void exit_help(boost::program_options::options_description desc)
+//{
+//  std::err << desc;
+//  exit(1);
+//}
+//
+//int main(int argc, char **argv)
+//{
+//  /* Parse aguments */
+//  namespace bpo = boost::program_options;
+//  bpo::options_description desc(bpo::options_description("Options"));
+//  bpo::variables_map vm;
+//
+//  /* Add options */
+//  desc.add_options()
+//    ("help,h", "Display the help")
+//    ("print,p", "Pretty printing of particules")
+//    ("time,t", "Evaluation of the performances in term of time")
+//
+//    ("topology,T", bpo::value<std::string>()->default_value("star"),
+//     "Changes the type of topology for the PSO algorithm")
+//
+//    ("video,v", bpo::value<std::string>()->default_value(""),
+//     "File path to video to analyse")
+//
+//    ("object,o", bpo::value<std::string>()->default_value(""),
+//     "File path to object to be detected")
+//
+//    ("frames,f", bpo::value<int>()->default_value(1),
+//     "Number of frames to analyse per second")
+//
+//    ("number,n", bpo::value<int>()->default_value(50),
+//     "Number of Particle per swarm");
+//
+//  /* Parse options */
+//  try
+//  {
+//    bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
+//    bpo::notify(vm);
+//  }
+//  catch (std::exception& e)
+//  {
+//    exit_help();
+//  }
+//
+//  /* Create PSO */
+//  Pso pso;
+//
+//  /* Set attributes depending on parsed input */
+//  /* Topology type */
+//  std::string topology(vm["topology"].as<std::string());
+//  topology ? pso.set_topology(topology) : pso.set_topology("star");;
+//
+//  /* Frames to analyze per second */
+//  int frames_per_second = vm["frames"].as<int>();
+//  if (frames_per_second < 0)
+//    exit_help();
+//  pso.set_frames_per_second(frames_per_second);
+//
+//  /* Input video */
+//  std::string path_to_video = vm["video"].as<string>();
+//  VideoManager video_manager = path_to_video == "" ?
+//    VideoManager() :
+//    VideoManager(path_to_video);
+//
+//
+//
+//  pso.launch_pso();
+//}
