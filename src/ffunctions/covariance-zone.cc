517aef35610ebced552945b765de7f8d73e1cdae
Nicolas Chariglione <nico.chari@gmail.com>
2016-07-05 07:14:01 +0200

    [FFUN ] Complete covariance zone ff

--- src/ffunctions/covariance-zone.cc
+++ src/ffunctions/covariance-zone.cc
@@ -15,26 +15,24 @@ namespace beetrail
 
   double CovarianceZone::operator()(Vector2 pos)
   {
-    /*if (frame_ != nullptr)
+    if (frame_ != nullptr)
     {
       cv::Mat subimage = Utilities::get_subimage(*frame_, pos, square_width,
           square_height);
-      subimage = Utilities::grayify(subimage);
+      MatrixXd matrix = compute_matrix(subimage);
 
-      MatrixXd matrix = transform_mat(subimage);
+      double d = std::abs(covariance_distance(matrix, image_covariant_matrix));
 
-      matrix = make_covariance_matrix(matrix);
-
-      return covariance_distance(matrix, image_covariant_matrix);
+      std::cout << "Distance = " << d << std::endl;
+      return d;
     }
-    std::cerr << "Frame is null. Cannot compute covariance zone" << std::endl;*/
+    std::cerr << "Frame is null. Cannot compute covariance zone" << std::endl;
 
     return 0;
   }
 
   MatrixXd CovarianceZone::compute_matrix(const cv::Mat& m)
   {
-    std::cout << "Image matrix : " << m << std::endl;
     MatrixXd mat = get_feature_mat(m);
     return make_covariance_matrix(mat);
   }
@@ -47,18 +45,10 @@ namespace beetrail
 
   MatrixXd CovarianceZone::make_covariance_matrix(MatrixXd m)
   {
-    std::cout << "===============================================" << std::endl;
-    std::cout << "make_covariance_matrix" << std::endl;
-    std::cout << "===============================================" << std::endl;
-    std::cout << std::endl;
-
-
-    std::cout << "Base matrix = " << m << std::endl << std::endl;
     MatrixXd centered = m.rowwise() - m.colwise().mean();
     if (m.rows() != 1)
     {
       MatrixXd cov = (centered.adjoint() * centered) / double(m.rows() - 1);
-      std::cout << "Cov matrix = " << std::endl << cov << std::endl;
 
       return cov;
     }
@@ -69,51 +59,33 @@ namespace beetrail
 
   MatrixXd CovarianceZone::get_feature_mat(const cv::Mat& image)
   {
-    std::cout << "===============================================" << std::endl;
-    std::cout << "get_feature_matrix" << std::endl;
-    std::cout << "===============================================" << std::endl;
-    std::cout << std::endl;
-
-
     /* Construct feature matrix with r, g, b, xgradient, ygradient */
     cv::Mat intensity_image = Utilities::grayify(image);
     cv::Mat x_gradient_image = get_x_gradient(intensity_image);
     cv::Mat y_gradient_image = get_y_gradient(intensity_image);
-    std::cout << " grey: " << std::endl;
-    std::cout << intensity_image << std::endl;
-    std::cout << " x gradient : " << std::endl;
-    std::cout << x_gradient_image << std::endl;
-    std::cout << " y gradient : " << std::endl;
-    std::cout << y_gradient_image << std::endl;
 
     int width = 6; /* R, G, B, I, Xg, Yg */
     int height = image.size().width * image.size().height;
 
     MatrixXd feature_matrix(height, width);
 
-    std::cout << "Feature matrix row by row construction: " << std::endl;
     int image_width = image.size().width;
     int image_height= image.size().height;
     for(int i = 0 ; i < image_width ; i++)
     {
       for (int j = 0 ; j < image_height ; j++)
       {
-        double b = (double) image.at<cv::Vec3b>(i, j)[0];
-        double g = (double) image.at<cv::Vec3b>(i, j)[1];
-        double r = (double) image.at<cv::Vec3b>(i, j)[2];
-        double intensity = (double) intensity_image.at<cv::Vec3b>(i, j)[0];
-        double x_grad = (double) x_gradient_image.at<cv::Vec3b>(i, j)[0];
-        double y_grad = (double) y_gradient_image.at<cv::Vec3b>(i, j)[0];
+        double b = (double) image.at<cv::Vec3b>(j, i)[0];
+        double g = (double) image.at<cv::Vec3b>(j, i)[1];
+        double r = (double) image.at<cv::Vec3b>(j, i)[2];
+        double intensity = (double) intensity_image.at<cv::Vec3b>(j, i)[0];
+        double x_grad = (double) x_gradient_image.at<cv::Vec3b>(j, i)[0];
+        double y_grad = (double) y_gradient_image.at<cv::Vec3b>(j, i)[0];
         RowVectorXd vec(width);
         vec << r, g, b, intensity, x_grad, y_grad;
-        std::cout << vec << std::endl;
         feature_matrix.row(image_height * i + j) << vec;
       }
     }
-
-    std::cout << std::endl;
-    std::cout << "Feature matrix : " << feature_matrix << std::endl;
-    std::cout << std::endl;
 
     return feature_matrix;
   }
