f0b3478000a174e1d4f2c33939f1b68a7ee959fc
Charles Yaiche <yaiche_c@epita.fr>
2016-07-06 07:18:53 +0000

    [CLEAN] main.cc : cleanning of the file

--- src/main.cc
+++ src/main.cc
@@ -38,9 +38,6 @@ int main(int argc, char **argv)
     ("print,p", "Pretty printing of particules")
     ("time,t", "Evaluation of the performances in term of time")
 
-    ("topology,T", bpo::value<std::string>()->default_value("star"),
-     "Changes the type of topology for the PSO algorithm")
-
     ("video,v", bpo::value<std::string>()->default_value(""),
      "File path to video to analyse")
 
@@ -69,27 +66,17 @@ int main(int argc, char **argv)
 
 
   /* Set attributes depending on parsed input */
-  /* Topology type */
-  //std::string topology(vm["topology"].as<std::string());
-  //topology ? pso.set_topology(topology) : pso.set_topology("star");;
-
-  /* Frames to analyze per second */
-  //int frames_per_second = vm["frames"].as<int>();
-  //if (frames_per_second < 0)
-   // exit_help();
-  //pso.set_frames_per_second(frames_per_second);
 
   /* Input image */
   std::string path_to_image = vm["image"].as<std::string>();
   cv::Mat image_to_detect = cv::imread(path_to_image, CV_LOAD_IMAGE_COLOR);
 
 
-
-
   /* Input video */
   std::string path_to_video = vm["video"].as<std::string>();
 
 
+  /* Instencialization of video manager class*/
   beetrail::VideoManager video_manager = path_to_video == "" ?
     beetrail::VideoManager() :
     beetrail::VideoManager(path_to_video);
@@ -103,39 +90,35 @@ int main(int argc, char **argv)
     std::ofstream benchmark_file(0);
 
 
+  /**/
+
   /* Main loop */
   int iteration_i = 1;
   int stop = 0;
   {
     cv::Mat frame = video_manager.frame_get();
 
-    /* Init PSO and fitness function */
-    /*beetrail::GrayScaleHistogram gs(image_to_detect, 100, &frame);
-    beetrail::Pso<beetrail::GrayScaleHistogram> pso(50, gs); */
     int nb_particles = vm["number"].as<int>();
+
+    /* Init PSO and fitness function */
+    /* Uncomment for grayscale histogramme fitness function use */
+      //beetrail::GrayScaleHistogram gs(image_to_detect, 100, &frame);
+      //beetrail::Pso<beetrail::GrayScaleHistogram> pso(50, gs); */
+    /* Covariance Zone fitness function */
     beetrail::CovarianceZone cz(image_to_detect, 100, &frame);
     beetrail::Pso<beetrail::CovarianceZone> pso(nb_particles, cz);
+
+
+    /* Initialization of the swarm */
     pso.init(frame.cols, frame.rows);
 
+    /* Timer initialization */
     double z = 0;
     std::ostringstream oss117;
     oss117 << "Global time: ";
     Timer global_timer(z, benchmark_file, oss117.str());
 
-    cv::VideoWriter output_vid;
-    int ex = static_cast<int>(video_manager.capture_get().get(CV_CAP_PROP_FOURCC));
-    
-    cv::Size S = Size(
-        (int) video_manager.capture_get().get(CV_CAP_PROP_FRAME_WIDTH),
-        (int) video_manager.capture_get().get(CV_CAP_PROP_FRAME_HEIGHT));
 
-    std::cout << "\n" << S;
-    output_vid.open("lol.mp4", -1,
-        video_manager.capture_get().get(CV_CAP_PROP_FPS), S, true);
-
-
-    if (!output_vid.isOpened())
-      std::cout << "\nfailed\n";
     while (!stop)
     {
       /* Iteration timer */
@@ -155,9 +138,10 @@ int main(int argc, char **argv)
       }
       video_manager.pretty_print(pso.list_particle_get(), frame);
 
-      output_vid << frame;
-      std::string stp = std::to_string(iteration_i);
-      cv::imwrite("img/arys/" +  stp + ".png", frame);
+
+      /* Uncomment to save each frame to a folder */
+      //std::string stp = std::to_string(iteration_i);
+      //cv::imwrite("img/arys/" +  stp + ".png", frame);
 
       video_manager.display_frame(frame, stop);
 
